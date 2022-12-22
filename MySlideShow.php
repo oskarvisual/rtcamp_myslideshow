5982ae56e5bfd70fc5971100e388cbff233b6f70
Oscar Fernandez <oskarvisual@gmail.com>
2022-12-22 18:02:37 -0500

    Plugin's setup and definition of constants

---
+++ MySlideShow.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+*
+* @package           MySlideShow
+* @author            Oscar Fernandez
+* @link              https://www.linkedin.com/in/oscarfer/
+* @since             1.0.0
+* @copyright         2022 MySlideShow
+* @license           GPL-3.0-or-later
+*
+* @wordpress-plugin
+* Plugin Name:       MySlideShow
+* Plugin URI:        https://rtcamp.com/assignments/wordpress-plugin/
+* Description:       This plugin test if I'm familiar with WordPress shortcodes.
+* Version:           1.0.0
+* Requires at least: 5.8
+* Requires PHP:      7.4
+* Author:            Oscar Fernandez
+* Author URI:        https://www.linkedin.com/in/oscarfer/
+* Text Domain:       myslideshow
+* License:           GPL v3 or later
+* License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+*/
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
+
+define( 'MYSLIDESHOW_VERSION', '1.0.0' );
+define( 'MYSLIDESHOW_NAME', 'myslideshow' );
+define( 'MYSLIDESHOW_MINIMUM_WP_VERSION', '5.0' );
+define( 'MYSLIDESHOW_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'MYSLIDESHOW_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
---
+++ readme.txt
@@ -0,0 +1,15 @@
+=== MySlideShow ===
+
+Contributors: Oscar Fernandez
+Tags: SlideShow
+Requires at least: 5.8
+Tested up to: 6.1.0
+Stable tag: 1.0.0
+Requires PHP: 7.4
+License: GPLv3 or later License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+
+This plugin test if I'm familiar with WordPress shortcodes.
+
+== Description ==
+
+[myslideshow]
