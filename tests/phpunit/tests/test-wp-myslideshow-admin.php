6d1689f404822f214ac58036e0ec384cbd35b6f0
Oscar Fernandez <oskarvisual@gmail.com>
2022-12-28 02:57:30 -0500

    Refactoring in Tests

--- tests/phpunit/tests/test-wp-myslideshow-admin.php
+++ tests/phpunit/tests/test-wp-myslideshow-admin.php
@@ -128,19 +128,76 @@ class Test_WP_MySlideshow_Admin extends WP_UnitTestCase {
 	 * It checks callbacks inputs
 	 */
 	public function test_form_callbacks():void {
+		$this->myslideshow_admin = new MySlideshow_Admin();
+
+		ob_start();
+		$this->myslideshow_admin->title_callback();
+		$field = ob_get_clean();
+
 		$this->assertSame(
-			sprintf(
-				'<input class="regular-text" type="text" name="myslideshow_options[myslideshow_title]" id="myslideshow_title" value="%s">',
-				isset( $this->myslideshow_options['myslideshow_title'] ) ? esc_attr( $this->myslideshow_options['myslideshow_title'] ) : ''
-			),
+			$field,
 			'<input class="regular-text" type="text" name="myslideshow_options[myslideshow_title]" id="myslideshow_title" value="My Slideshow">'
 		);
 
-		$this->assertSame( '<input type="button" value="Upload Images" class="button-primary" id="upload_image"/>', '<input type="button" value="Upload Images" class="button-primary" id="upload_image"/>' );
+		ob_start();
+		$this->myslideshow_admin->upload_callback();
+		$field = ob_get_clean();
+
+		$this->assertSame( 
+			$field, 
+			'<input type="button" value="Upload Images" class="button-primary" id="upload_image"/>' 
+		);
 
 		$this->assertCount( 0, json_decode( $this->myslideshow_options['myslideshow_images'] ) );
 
-		$html_input = '<ul class="myslideshow" id="slider">';
+		ob_start();
+		$this->myslideshow_admin->slider_callback();
+		$field = ob_get_clean();
+		
+		$this->assertSame( 
+			$field, 
+			'<ul class="myslideshow" id="slider"></ul>'
+		);
+
+		$images = array(
+			media_sideload_image( 'https://loremflickr.com/cache/resized/65535_51919515453_200d29dcae_320_240_nofilter.jpg', null, null, 'id' ),
+			media_sideload_image( 'https://loremflickr.com/cache/resized/65535_52215297981_6ca7bb37e1_n_320_240_nofilter.jpg', null, null, 'id' ),
+			media_sideload_image( 'https://loremflickr.com/cache/resized/65535_51909301278_0817277668_n_320_240_nofilter.jpg', null, null, 'id' ),
+		);
+
+		$this->assertCount( 3, $images );
+
+		$input = array(
+			'myslideshow_title'  => 'My Slideshow<h1><?php evil ?>',
+			'myslideshow_images' => array(
+				array(
+					'id'    => $images[0],
+					'title' => 'Title 1',
+					'alt'   => 'Alt 1',
+					'url'   => 'https://google.com',
+				),
+				array(
+					'id'    => $images[1],
+					'title' => 'Title 2',
+					'alt'   => 'Alt 2',
+					'url'   => 'https://google.com',
+				),
+				array(
+					'id'    => $images[2],
+					'title' => 'Title 3',
+					'alt'   => 'Alt 2',
+					'url'   => 'https://google.com',
+				),
+			),
+		);
+
+		$output = $this->myslideshow_admin->sanitize_options( $input );
+
+		ob_start();
+		$this->myslideshow_admin->slider_callback();
+		$field = ob_get_clean();
+
+		$field_2 = '<ul class="myslideshow" id="slider">';
 
 		if ( isset( $this->myslideshow_options['myslideshow_images'] ) ) {
 
@@ -148,79 +205,82 @@ class Test_WP_MySlideshow_Admin extends WP_UnitTestCase {
 
 			if ( is_array( $images ) ) {
 				foreach ( $images as $key => $image ) {
-					$html_input .= sprintf( '<li class="myslideshow__item" id="slider-item-%s">', esc_attr( $image['id'] ) );
+					$field_2 .= sprintf( '<li class="myslideshow__item" id="slider-item-%s">', esc_attr( $image['id'] ) );
 
-					$html_input .= '<span class="myslideshow__move dashicons dashicons-move"></span>';
+					$field_2 .= '<span class="myslideshow__move dashicons dashicons-move"></span>';
 
-					$html_input .= sprintf( '<span class="myslideshow__delete dashicons dashicons-trash" data-id="%s"></span>', esc_attr( $image['id'] ) );
+					$field_2 .= sprintf( '<span class="myslideshow__delete dashicons dashicons-trash" data-id="%s"></span>', esc_attr( $image['id'] ) );
 
-					$html_input .= '<div class="myslideshow__content">';
+					$field_2 .= '<div class="myslideshow__content">';
 
-					$html_input .= '<div class="myslideshow__thumbnail">';
+					$field_2 .= '<div class="myslideshow__thumbnail">';
 
-					$html_input .= sprintf( '<img src="%s" class="myslideshow__image" />', esc_attr( wp_get_attachment_image_url( intval( $image['id'] ), 'thumbnail', false ) ) );
+					$field_2 .= sprintf( '<img src="%s" class="myslideshow__image" />', esc_attr( wp_get_attachment_image_url( intval( $image['id'] ), 'thumbnail', false ) ) );
 
-					$html_input .= '</div>';
+					$field_2 .= '</div>';
 
-					$html_input .= '<div class="myslideshow__form">';
+					$field_2 .= '<div class="myslideshow__form">';
 
-					$html_input .= sprintf( '<input type="hidden" name="myslideshow_options[myslideshow_images][%s][id]" value="%s" />', esc_attr( $image['id'] ), esc_attr( $image['id'] ) );
+					$field_2 .= sprintf( '<input type="hidden" name="myslideshow_options[myslideshow_images][%s][id]" value="%s" />', esc_attr( $image['id'] ), esc_attr( $image['id'] ) );
 
-					$html_input .= '<table class="form-table" role="presentation">';
+					$field_2 .= '<table class="form-table" role="presentation">';
 
-					$html_input .= '<tbody>';
+					$field_2 .= '<tbody>';
 
-					$html_input .= '<tr>';
+					$field_2 .= '<tr>';
 
-					$html_input .= '<th scope="row">Image Title Text</th>';
+					$field_2 .= '<th scope="row">Image Title Text</th>';
 
-					$html_input .= '<td>';
+					$field_2 .= '<td>';
 
-					$html_input .= sprintf( '<input class="regular-text" type="text" name="myslideshow_options[myslideshow_images][%s][title]" value="%s" />', esc_attr( $image['id'] ), esc_attr( $image['title'] ) );
+					$field_2 .= sprintf( '<input class="regular-text" type="text" name="myslideshow_options[myslideshow_images][%s][title]" value="%s" />', esc_attr( $image['id'] ), esc_attr( $image['title'] ) );
 
-					$html_input .= '</td>';
+					$field_2 .= '</td>';
 
-					$html_input .= '</tr>';
+					$field_2 .= '</tr>';
 
-					$html_input .= '<tr>';
+					$field_2 .= '<tr>';
 
-					$html_input .= '<th scope="row">Image Alt Text</th>';
+					$field_2 .= '<th scope="row">Image Alt Text</th>';
 
-					$html_input .= '<td>';
+					$field_2 .= '<td>';
 
-					$html_input .= sprintf( '<input class="regular-text" type="text" name="myslideshow_options[myslideshow_images][%s][alt]" value="%s" />', esc_attr( $image['id'] ), esc_attr( $image['alt'] ) );
+					$field_2 .= sprintf( '<input class="regular-text" type="text" name="myslideshow_options[myslideshow_images][%s][alt]" value="%s" />', esc_attr( $image['id'] ), esc_attr( $image['alt'] ) );
 
-					$html_input .= '</td>';
+					$field_2 .= '</td>';
 
-					$html_input .= '</tr>';
+					$field_2 .= '</tr>';
 
-					$html_input .= '<tr>';
+					$field_2 .= '<tr>';
 
-					$html_input .= '<th scope="row">Link URL (optional)</th>';
+					$field_2 .= '<th scope="row">Link URL (optional)</th>';
 
-					$html_input .= '<td>';
+					$field_2 .= '<td>';
 
-					$html_input .= sprintf( '<input class="regular-text" type="text" name="myslideshow_options[myslideshow_images][%s][url]" value="%s" />', esc_attr( $image['id'] ), esc_attr( $image['url'] ) );
+					$field_2 .= sprintf( '<input class="regular-text" type="text" name="myslideshow_options[myslideshow_images][%s][url]" value="%s" />', esc_attr( $image['id'] ), esc_attr( $image['url'] ) );
 
-					$html_input .= '</td>';
+					$field_2 .= '</td>';
 
-					$html_input .= '</tr>';
+					$field_2 .= '</tr>';
 
-					$html_input .= '</tbody>';
+					$field_2 .= '</tbody>';
 
-					$html_input .= '</table>';
+					$field_2 .= '</table>';
 
-					$html_input .= '</div>';
+					$field_2 .= '</div>';
 
-					$html_input .= '</div>';
+					$field_2 .= '</div>';
 
-					$html_input .= '</li>';
+					$field_2 .= '</li>';
 				}
 			}
 		}
 
-		$html_input .= '</ul>';
-
-		$this->assertSame( $html_input, '<ul class="myslideshow" id="slider"></ul>' );
+		$field_2 .= '</ul>';
+		
+		$this->assertSame( 
+			$field, 
+			$field_2
+		);
 	}
 }
--- tests/phpunit/tests/test-wp-myslideshow-shortcode.php
+++ tests/phpunit/tests/test-wp-myslideshow-shortcode.php
@@ -43,5 +43,14 @@ class Test_WP_MySlideshow_Shortcode extends WP_UnitTestCase {
 	 */
 	public function test_shortcode() {
 		$this->assertIsString( $this->myslideshow_shortcode->shortcode( array() ) );
+		
+		$attributes = array(
+			'slides' => 1,
+			'title'  => 1,
+			'arrows' => 1,
+			'dots'   => 1,
+		);
+
+		$this->assertSame( $this->myslideshow_shortcode->shortcode( $attributes ), '<div class="glider-contain">' . sprintf( '<div class="glider myslideshow" id="myslideshow" data-slides="%s" data-arrows="%s" data-dots="%s">', esc_attr( $attributes['slides'] ), esc_attr( $attributes['arrows'] ), esc_attr( $attributes['dots'] ) ) . '</div><button aria-label="Previous" class="glider-prev myslideshow__button myslideshow__button--prev" id="myslideshow-prev"><span class="dashicons dashicons-arrow-left-alt2"></span></button><button aria-label="Next" class="glider-next myslideshow__button myslideshow__button--next" id="myslideshow-next"><span class="dashicons dashicons-arrow-right-alt2"></span></button><div role="tablist" class="glider-dots myslideshow__dots" id="myslideshow-dots"></div></div>' );
 	}
 }
